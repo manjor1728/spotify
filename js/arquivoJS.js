021dbeb77ec52d6c4ce1cb3f4ce0b7cdf97fe319
Narciso Manjor <nlmanjor1728@gmail.com>
2023-11-26 16:40:01 +0000

    footer
    terminei de fazer o acordion da secção duvida, posteriormente fiz a inclusão do rodapé

--- js/arquivoJS.js
+++ js/arquivoJS.js
@@ -7,42 +7,21 @@ botaoMenu.addEventListener('click', ()=>{
 
 
 
-/* const pergunta1 = document.querySelector('.per')
 
-pergunta1.addEventListener('click', ()=>{
-    const resposta = document.querySelector('.resposta')
-    const btnUp = document.querySelector('.up')
-    const btnDown = document.querySelector('.down')
 
-    resposta.classList.toggle('respostaSumir')
-    btnUp.classList.toggle('botaoUp')
-    btnDown.classList.toggle('botaoDown')
-    console.log('clicou')
-}) */
-/* const per2 = document.querySelector('.per2')
 
-per2.addEventListener('click', ()=>{
-    const resposta = document.querySelector('.resposta2')
-    const btnUp = document.querySelector('.up2')
-    const btnDown = document.querySelector('.down2')
+const acordionItem = document.querySelectorAll('.acordionItem')
 
-    resposta.classList.toggle('respostaSumir2')
-    btnUp.classList.toggle('botaoUp2')
-    btnDown.classList.toggle('botaoDown2')
-    console.log('clicou')
-}) */
-const pergunta1 = document.querySelectorAll('.pergunta1')
-
-pergunta1.forEach(cadaPergunta =>{
-    cadaPergunta.addEventListener('click', ()=>{
+acordionItem.forEach(item =>
+    item.addEventListener('click', ()=>{
         
-        const resposta = cadaPergunta.classList.contains('respostaSumir')
+        const isItemRespostaSumir = item.classList.contains('respostaSumir')
 
-        pergunta1.forEach(cadaPergunta =>{
-            cadaPergunta.classList.remove('respostaSumir')
-            if (!resposta) {
-                cadaPergunta.classList.toggle('respostaSumir')
-            }
-        })
+        item.classList.remove('respostaSumir')
+      
+        if (!isItemRespostaSumir) {
+            item.classList.toggle('respostaSumir')
+        
+        }
     })
-})
+)
